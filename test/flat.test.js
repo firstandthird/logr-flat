99f9e7944a1b592141c5c9c20f5e16edcee8d557
Chris <orthagonal1@gmail.com>
2017-04-07 18:20:27 -0500

    doesn't verify colors in test

--- test/flat.test.js
+++ test/flat.test.js
@@ -41,30 +41,3 @@ test('can use the flat plugin to print an object as one log line', (t) => {
   t.equal(logs.length, 1, 'prints all on one line');
   t.end();
 });
-
-test('can use the flat plugin to print an object with colorful keys', (t) => {
-  const oldConsole = console.log;
-  const logs = [];
-  console.log = (data) => {
-    logs.push(data);
-  };
-  const log = Logr.createLogger({
-    type: 'flat',
-    reporters: {
-      flat: {
-        reporter: logrFlat,
-        colors: true
-      }
-    }
-  });
-  log({
-    a: true,
-    b: false,
-    hi: 'there',
-    hello: 'goodbye',
-    number: 9
-  });
-  console.log = oldConsole;
-  t.equal(logs.length, 1, 'prints all on one line');
-  t.end();
-});
